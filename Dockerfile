277f8b974e2aacbe0e3332e02f3fd0742d678f02
Chisomo Sakala <chisomo.sakala@mombo.solution>
2016-08-06 05:18:32 -0500

    initial commit in memory library

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,23 @@
-FROM node:latest
+FROM node:4.4.7-wheezy
 
-
+# Bundle app source
 RUN mkdir -p /usr/src/app
 WORKDIR /usr/src/app
-# Bundle app source
 COPY . /usr/src/app
-
 VOLUME ["/usr/src/app"]
 
 
+# Install naitive dependencies
+RUN apt-get update && apt-get upgrade -y  && apt-get install -y python-software-properties \ 
+  && add-apt-repository "deb http://http.debian.net/debian wheezy-backports main" \  
+  && apt-get update && apt-get install -y libcairo2-dev \ 
+  && rm -rf /var/lib/apt/lists/*
 
-# Install app dependencies
-RUN apt-get update && apt-get install -y \
-    pdftk \
- && rm -rf /var/lib/apt/lists/*
-RUN npm install --save express pdffiller
+# Install node dependencies  
+RUN npm install -g node-gyp
+RUN npm install --save \ 
+	express \ 
+	pdf-fill-form
 
 
 EXPOSE 3000
--- index.js
+++ index.js
@@ -1,34 +1,46 @@
-const express = require('express');
+var fs = require('fs');
 
-// Constants
-const PORT = 3000;
-
-// App
-const app = express();
-app.get('/', function (req, res) {
-  res.send('Hello momo lolo sakala hey\n');
-});
-
-const pdfFiller   = require('pdffiller');
-
-var sourcePDF = "test/test.pdf";
-var destinationPDF =  "test/test_complete.pdf";
-var shouldFlatten = true;
-var data = {
-    "last_name" : "John",
-    "first_name" : "Doe",
-    "date" : "Jan 1, 2013",
+var pdfFillForm = require('pdf-fill-form');
+var req_body_data = {
+    "last_name" : "Momo",
+    "first_name" : "Sakala" /*,
+    "date" : "Jan 1, 1988,
     "football" : "Off",
     "baseball" : "Yes",
     "basketball" : "Off",
     "hockey" : "Yes",
     "nascar" : "Off"
-};
+		*/
+};	 
 
-pdfFiller.fillFormWithFlatten( sourcePDF, destinationPDF, data, shouldFlatten, function(err) {
-    if (err) throw err;
-    console.log("In callback (we're done).");
-}); 
 
+	
+/*-------- Web Serve ---------*/
+const app = require('express')();
+app.get('/', function (req, res) {
+
+	var pdfFields = pdfFillForm.readSync('test/test.pdf');
+	output['pdfFields'] = pdfFields;
+	res.contentType("application/json");
+  res.send(JSON.stringify(output));
+});
+
+app.get('/post', function (req, res) {
+
+
+	pdfFillForm.writeAsync('test/test.pdf', 
+			req_body_data, { "save": "pdf" }, 
+			function(err, pdf) {
+					//fs.writeFile("test/filled_test.pdf", pdf, function(err){});
+						res.contentType("application/pdf");
+						res.send(pdf);
+			}
+	);	
+		
+	
+
+});
+
+const PORT = 3000;
 app.listen(PORT);
 console.log('Running on http://localhost:' + PORT);
